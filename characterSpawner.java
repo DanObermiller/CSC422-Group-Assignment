9966cc9465b2dccb281bfef8f15f36895a10f811
DanObermiller <charrcorncob@gmail.com>
2026-02-11 14:42:09 -0600

    Update characterSpawner.java

--- characterSpawner.java
+++ characterSpawner.java
@@ -1,1 +1,29 @@
+package zombieWar;
 
+import java.util.Random;
+
+public class characterSpawner {
+    public Survivor spawnHuman(int survivorCount)
+    {
+        Random rand = new Random();
+        int randInt = rand.nextInt(3);
+        Survivor human;
+        
+        if (randInt == 0)
+        {
+            human = new Soldier();
+            
+        }
+        else if (randInt == 1)
+        {
+            human = new Child();
+        }
+        else {
+            human = new Teacher();
+        }
+        
+        String name = human.getName();
+        human.setName("" + name + " " + (survivorCount + 1));
+        return human;
+    }
+}
