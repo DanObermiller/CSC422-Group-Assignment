dafa0f6f6f909e6cad50bf97bf21a128294caddc
DanObermiller <charrcorncob@gmail.com>
2026-02-11 14:50:03 -0600

    Update characterSpawner.java

--- characterSpawner.java
+++ characterSpawner.java
@@ -26,4 +26,31 @@ public class characterSpawner {
         human.setName("" + name + " " + (survivorCount + 1));
         return human;
     }
+    
+    public Survivor[] spawnManyHuman(int amount, int survivorCount)
+    {
+        Random rand = new Random();
+        int randInt = rand.nextInt(3);
+        Survivor[] humans;
+        for (int i = 0; i < amount; i++)
+        {
+            if (randInt == 0)
+            {
+                humans[i] = new Soldier();
+
+            }
+            else if (randInt == 1)
+            {
+                humans[i] = new Child();
+            }
+            else {
+                humans[i] = new Teacher();
+            }
+
+            String name = humans[i].getName();
+            humans[i].setName("" + name + " " + (survivorCount + 1 + i));
+            randInt = rand.nextInt(3);
+        }
+        return humans[];
+    }
 }
